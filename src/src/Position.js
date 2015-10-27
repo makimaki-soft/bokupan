9f5710ad870808d1a2080562ae23f1ecde3b9263
over-hrk <over.hrk@gmail.com>
2015-10-28 00:40:57 +0900

    家間の道のマスを追加

--- src/src/Position.js
+++ src/src/Position.js
@@ -37,21 +37,33 @@ var POSITION_ID = {
   ALLOW_2 : 14,
   ALLOW_3 : 15,
   ALLOW_4 : 16,
-  NUM_POSISION : 17
+  ROAD_1  : 17,
+  ROAD_2  : 18,
+  ROAD_3  : 19,
+  ROAD_4  : 20,
+  ROAD_5  : 21,
+  ROAD_6  : 22,
+  ROAD_7  : 23,
+  ROAD_8  : 24,
+  ROAD_9  : 25,
+  ROAD_10  : 26,
+  ROAD_11  : 27,
+  ROAD_12  : 28,
+  NUM_POSISION : 29
 }
 
 // Positional relationship of the house to each other
 var HOME_Relation = [
   //             LEFT,              ABOVE,              RIGHT,              BELOW
-  [POSITION_ID.HOME_A, POSITION_ID.HOME_4, POSITION_ID.HOME_2,               null],   // HOME_1
-  [POSITION_ID.HOME_1, POSITION_ID.HOME_5, POSITION_ID.HOME_3,               null],   // HOME_2
-  [POSITION_ID.HOME_2, POSITION_ID.HOME_6, POSITION_ID.HOME_B,               null],   // HOME_3
-  [              null, POSITION_ID.HOME_7, POSITION_ID.HOME_5, POSITION_ID.HOME_1],   // HOME_4
-  [POSITION_ID.HOME_4, POSITION_ID.HOME_8, POSITION_ID.HOME_6, POSITION_ID.HOME_2],   // HOME_5
-  [POSITION_ID.HOME_5, POSITION_ID.HOME_9,               null, POSITION_ID.HOME_3],   // HOME_6
-  [POSITION_ID.HOME_C,               null, POSITION_ID.HOME_8, POSITION_ID.HOME_4],   // HOME_7
-  [POSITION_ID.HOME_7,               null, POSITION_ID.HOME_9, POSITION_ID.HOME_5],   // HOME_8
-  [POSITION_ID.HOME_8,               null, POSITION_ID.HOME_D, POSITION_ID.HOME_6],   // HOME_9
+  [POSITION_ID.HOME_A, POSITION_ID.ROAD_3 , POSITION_ID.ROAD_1,               null],   // HOME_1
+  [POSITION_ID.ROAD_1, POSITION_ID.ROAD_4 , POSITION_ID.ROAD_2,               null],   // HOME_2
+  [POSITION_ID.ROAD_2, POSITION_ID.ROAD_5 , POSITION_ID.HOME_B,               null],   // HOME_3
+  [              null, POSITION_ID.ROAD_8 , POSITION_ID.ROAD_6, POSITION_ID.ROAD_3],   // HOME_4
+  [POSITION_ID.ROAD_6, POSITION_ID.ROAD_9 , POSITION_ID.ROAD_7, POSITION_ID.ROAD_4],   // HOME_5
+  [POSITION_ID.ROAD_7, POSITION_ID.ROAD_10,               null, POSITION_ID.ROAD_5],   // HOME_6
+  [POSITION_ID.HOME_C,               null, POSITION_ID.ROAD_11, POSITION_ID.ROAD_8],   // HOME_7
+  [POSITION_ID.ROAD_11,              null, POSITION_ID.ROAD_12, POSITION_ID.ROAD_9],   // HOME_8
+  [POSITION_ID.ROAD_12,              null, POSITION_ID.HOME_D, POSITION_ID.ROAD_10],   // HOME_9
   [              null,               null, POSITION_ID.HOME_1,               null],   // HOME_A
   [POSITION_ID.HOME_3,               null,               null,               null],   // HOME_B
   [              null,               null, POSITION_ID.HOME_7,               null],   // HOME_C
@@ -60,6 +72,18 @@ var HOME_Relation = [
   [          DIR.LEFT,             DIR.UP,          DIR.RIGHT,               null],   // ALLOW_2
   [          DIR.LEFT,               null,          DIR.RIGHT,           DIR.DOWN],   // ALLOW_3
   [          DIR.LEFT,             DIR.UP,               null,           DIR.DOWN],   // ALLOW_4
+  [POSITION_ID.HOME_1,               null, POSITION_ID.HOME_2,               null],   // ROAD_1
+  [POSITION_ID.HOME_2,               null, POSITION_ID.HOME_3,               null],   // ROAD_2
+  [              null, POSITION_ID.HOME_3,               null, POSITION_ID.HOME_1],   // ROAD_3
+  [              null, POSITION_ID.HOME_5,               null, POSITION_ID.HOME_2],   // ROAD_4
+  [              null, POSITION_ID.HOME_6,               null, POSITION_ID.HOME_3],   // ROAD_5
+  [POSITION_ID.HOME_4,               null, POSITION_ID.HOME_5,               null],   // ROAD_6
+  [POSITION_ID.HOME_5,               null, POSITION_ID.HOME_6,               null],   // ROAD_7
+  [              null, POSITION_ID.HOME_7,               null, POSITION_ID.HOME_4],   // ROAD_8
+  [              null, POSITION_ID.HOME_8,               null, POSITION_ID.HOME_5],   // ROAD_9
+  [              null, POSITION_ID.HOME_9,               null, POSITION_ID.HOME_6],   // ROAD_10
+  [POSITION_ID.HOME_7,               null, POSITION_ID.HOME_8,               null],   // ROAD_11
+  [POSITION_ID.HOME_8,               null, POSITION_ID.HOME_9,               null],   // ROAD_12
 ]
 
 var Coordinate = [
@@ -80,6 +104,18 @@ var Coordinate = [
   , {x:239, y: 39}  // ALLOW_2
   , {x:165, y:370}  // ALLOW_3
   , {x:365, y:233}  // ALLOW_4
+  , {x:145, y: 90}  // ROAD_1
+  , {x:255, y: 90}  // ROAD_2
+  , {x: 90, y:145}  // ROAD_3
+  , {x:200, y:145}  // ROAD_4
+  , {x:309, y:145}  // ROAD_5
+  , {x:145, y:200}  // ROAD_6
+  , {x:255, y:200}  // ROAD_7
+  , {x: 90, y:255}  // ROAD_8
+  , {x:200, y:255}  // ROAD_9
+  , {x:309, y:255}  // ROAD_10
+  , {x:145, y:310}  // ROAD_11
+  , {x:255, y:310}  // ROAD_12
 ];  
 
 
