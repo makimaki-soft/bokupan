582009e16dc640b2ae8006884bc61b36f8e5aac0
over-hrk <over.hrk@gmail.com>
2015-12-31 22:33:08 +0900

    Mkmk_MenuItemImageを別ファイルに分離

--- server_side/bokupan_ss/webroot/src/Phase.js
+++ server_side/bokupan_ss/webroot/src/Phase.js
@@ -1,30 +1,3 @@
-var Mkmk_MenuItemImage = cc.MenuItemImage.extend({
-    ctor:function (normalImage, selectedImage, label, callback, target) {
-        this.target = target;
-        this.CallBackOrg = function(){
-          rtc_manager.send(rtc_helper.encode(label, {}));
-          cc.eventManager.dispatchCustomEvent(label);
-        };
-        
-        this._super(normalImage, selectedImage, this.CallBackOrg, target);
-    }
-  
- , addCallbackFunc:function(  fnc,    // callBack
-                              target, // "this" of callBack
-                              id      // condition ID
- ){
-      this.setCallback(function(sender){
-          // sender には setCallback をcallしたオブジェクトが入る
-          if(sender.CallBackOrg){
-            // コンストラクタ呼び出し時のtargetでcallBackをcall
-            sender.CallBackOrg.call(this);
-          }
-          // 追加のcallBackをcall
-          fnc.call(target, id, 0);
-      },this.target);
-  } 
-});
-
 var playCnt = 2;
 
 function Mkmk_Phase() {
